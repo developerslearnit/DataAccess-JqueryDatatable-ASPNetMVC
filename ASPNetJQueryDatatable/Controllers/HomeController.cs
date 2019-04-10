fd1298d79217270b02837e35bb0df215556af1fe
Adesina Mark <developerslearnit@gmail.com>
2019-04-10 23:04:58 +0100

    Completed code

--- ASPNetJQueryDatatable/Controllers/HomeController.cs
+++ ASPNetJQueryDatatable/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ASPNetJQueryDatatable.Models;
+using ASPNetJQueryDatatable.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,42 @@ namespace ASPNetJQueryDatatable.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        DatatableService _srv;
+        public HomeController()
+        {
+            _srv = new DatatableService();
+        }
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult loadCustomers()
+        {
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int skip = start != null ? Convert.ToInt32(start) : 0;
+            int recordsTotal = 0;
+
+            var model = _srv.GetCustomers();
+
+            recordsTotal = model.Count();
+
+
+            var data = model.OrderBy(x => x.CUST_CODE).Skip(skip).Take(pageSize);
+
+            return Json(new
+            {
+                draw = draw,
+                recordsFiltered = recordsTotal,
+                recordsTotal = recordsTotal,
+                data = data
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ ASPNetJQueryDatatable/Services/DatatableService.cs
@@ -0,0 +1,22 @@
+﻿using ASPNetJQueryDatatable.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNetJQueryDatatable.Services
+{
+    public class DatatableService
+    {
+        DatatableEntities _db;
+        public DatatableService()
+        {
+            _db = new DatatableEntities();
+        }
+
+        public IQueryable<CUSTOMER> GetCustomers()
+        {
+            return _db.CUSTOMERs;
+        }
+    }
+}
